56d65de5e970991280781f21fb42e1a4251c6ab6
Carlox33 <c4rloxmartox@gmail.com>
2026-06-03 17:09:37 +0200

    +Soporte robusto para fullscreen

--- android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -49,6 +49,8 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 
 import java.util.Hashtable;
 import java.util.Locale;
@@ -418,9 +420,9 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
 
         setContentView(mLayout);
 
-        setWindowStyle(false);
-
         getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(this);
+
+        applySHARFullscreen();
 
         // Get filename from "Open with" of another application
         Intent intent = getIntent();
@@ -474,6 +476,8 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
         Log.v(TAG, "onResume()");
         super.onResume();
 
+        applySHARFullscreen();
+        
         if (mHIDDeviceManager != null) {
             mHIDDeviceManager.setFrozen(false);
         }
@@ -541,6 +545,7 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
 
         mHasFocus = hasFocus;
         if (hasFocus) {
+           applySHARFullscreen();
            mNextNativeState = NativeState.RESUMED;
            SDLActivity.getMotionListener().reclaimRelativeMouseModeIfNeeded();
 
@@ -782,16 +787,7 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
                         Window window = ((Activity) context).getWindow();
                         if (window != null) {
                             if ((msg.obj instanceof Integer) && ((Integer) msg.obj != 0)) {
-                                int flags = View.SYSTEM_UI_FLAG_FULLSCREEN |
-                                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.INVISIBLE;
-                                window.getDecorView().setSystemUiVisibility(flags);
-                                window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-                                window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-                                SDLActivity.mFullscreenModeActive = true;
+                            ((SDLActivity) context).applySHARFullscreen();
                             } else {
                                 int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_VISIBLE;
                                 window.getDecorView().setSystemUiVisibility(flags);
@@ -951,6 +947,73 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
         // Called from SDLMain() thread and can't directly affect the view
         return mSingleton.sendCommand(COMMAND_CHANGE_TITLE, title);
     }
+
+    private void applySHARFullscreen() {
+    final Window window = getWindow();
+
+    if (window == null) {
+        return;
+    }
+
+    /*
+     * Window-level fullscreen flags.
+     */
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+    /*
+     * Allow drawing into notch/cutout areas on Android 9+.
+     */
+    if (Build.VERSION.SDK_INT >= 28 /* Android 9 Pie */) {
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.layoutInDisplayCutoutMode =
+                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        window.setAttributes(params);
+    }
+
+    final View decorView = window.getDecorView();
+
+    if (decorView == null) {
+        return;
+    }
+
+    /*
+     * Android 11+ modern immersive fullscreen path.
+     */
+    if (Build.VERSION.SDK_INT >= 30 /* Android 11 */) {
+        window.setDecorFitsSystemWindows(false);
+
+        WindowInsetsController controller = decorView.getWindowInsetsController();
+
+        if (controller != null) {
+            controller.hide(WindowInsets.Type.statusBars());
+            controller.hide(WindowInsets.Type.navigationBars());
+
+            controller.setSystemBarsBehavior(
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            );
+        }
+    }
+
+    /*
+     * Legacy immersive sticky path.
+     * We keep this even on newer Android versions because it is harmless and
+     * helps some OEM implementations keep the decor flags consistent.
+     */
+    if (Build.VERSION.SDK_INT >= 19 /* Android 4.4 KitKat */) {
+        int flags = View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+
+        decorView.setSystemUiVisibility(flags);
+    }
+
+    SDLActivity.mFullscreenModeActive = true;
+}
 
     /**
      * This method is called by SDL using JNI.
@@ -1636,16 +1699,7 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
     private final Runnable rehideSystemUi = new Runnable() {
         @Override
         public void run() {
-            if (Build.VERSION.SDK_INT >= 19 /* Android 4.4 (KITKAT) */) {
-                int flags = View.SYSTEM_UI_FLAG_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.INVISIBLE;
-
-                SDLActivity.this.getWindow().getDecorView().setSystemUiVisibility(flags);
-            }
+            applySHARFullscreen();
         }
     };
 
